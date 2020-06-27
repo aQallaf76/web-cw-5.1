deb66801e06144d5d0462aa521cfbe1a70720faa
aQallaf76 <66533804+aQallaf76@users.noreply.github.com>
2020-06-27 20:40:22 +0300

    DONE

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,27 +1,37 @@
 // السؤال الأول
 // 1
-
+const name = "عبدالله";
 // 2
-
+const age = 18;
 // 3 and 4
-
+console.log("أنا" + name + "وعمري" + age);
 // 5 and 6
-
+console.log(age * 5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobies = ["gaming", "travelling", "shooting"]
 // 2
-
+hobies.push(age);
+console.log(hobies) + (age);
 // 3
-
+hobies.pop();
+console.log(hobies);
 // 4
+let web = {
+    favlang: "css",
+    msary: "web development",
+    teacher: "mnawer",
+    TA: ["nancy", "rahaf", "hassan", "moudhi"],
+};
 
 // 5
+console.log(web.msary);
+
 
 // 6
-
+console.log(web.TA[3]);
 // 7
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
